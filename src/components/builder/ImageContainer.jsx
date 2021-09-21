2e6f0260e0d9c580a804c89243a90700bc01fa67
jaytimothee <timotheej@outlook.com>
2021-09-21 02:59:38 -0400

    view

--- src/components/builder/ImageContainer.jsx
+++ src/components/builder/ImageContainer.jsx
@@ -8,6 +8,7 @@ const ImageContainer = () => {
     <div className="image-container">
       <div className="image-wrapper">
         <img className="flex-image" src={sweatpants} alt="sweatpants" />
+        <img className="flex-image2" src="https://www.vhv.rs/dpng/d/420-4202050_pixel-art-shovel-knight-hd-png-download.png" />
       </div>
     </div >
   );
