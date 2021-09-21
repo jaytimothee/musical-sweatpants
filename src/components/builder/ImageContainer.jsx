dd2a9b4bef4bc5c5b4ce1ce13438ec3b7037b2a9
jaytimothee <timotheej@outlook.com>
2021-09-21 02:09:26 -0400

    add sweatpants image

--- src/components/builder/ImageContainer.jsx
+++ src/components/builder/ImageContainer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import sweatpants from '../../assets/sweatpants.png'
+import "../../css/ImageContainer.css";
 
 const ImageContainer = () => {
 
   return (
-    <div>
-
+    <div className="image-container">
+      <div className="image-wrapper">
+        <img className="flex-image" src={sweatpants} alt="sweatpants" />
+      </div>
     </div >
   );
 };
